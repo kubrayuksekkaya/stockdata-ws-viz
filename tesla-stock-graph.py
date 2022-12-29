6eaff0bcaf38db1dab6dd77a37870c38f42a626d
Kübra Yüksekkaya <91791736+kubrayuksekkaya@users.noreply.github.com>
2022-12-29 17:37:46 +0000

    Visualizing Tesla Stock Data with Matplotlib
    In this code example, we use the matplotlib library to plot a graph of the Tesla stock data. We define a make_graph function that takes a dataframe and a title as input and creates a figure and a subplot using the plot function. We then set the title of the graph using the set_title function. This code is a useful starting point for anyone interested in visualizing and understanding the trends in Tesla's stock price over time.

---
+++ tesla-stock-graph.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+def make_graph(data, title):
+    # Get the date and close price columns
+    dates = data["Date"]
+    close_prices = data["Close"]
+
+    # Create a figure and a subplot
+    fig, ax = plt.subplots()
+
+    # Plot the close prices
+    ax.plot(dates, close_prices)
+
+    # Set the title of the graph
+    ax.set_title(title)
+    
+make_graph(tesla_data, "Tesla Stock Price")
